d0b6bd5adab3b9ea63a80526108c14187f7b6bfc
wangzekai <wangzekai.parallel@bytedance.com>
2023-11-02 10:18:31 +0800

    Expand xmake options to control compile.

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,35 @@
+set_project("pigeon_engine")
+set_version("0.1.0")
+set_xmakever("2.8.3")
+
 set_languages("cxx20")
+set_warnings("all", "error")
+
+option("test")
+    set_default(false)
+    set_description("Enable unittest")
+option_end()
+
+option("examples")
+    set_default(false)
+    set_description("Enable examples")
+option_end()
 
 add_rules("mode.debug", "mode.release")
 
 target("pigeon_engine")
-    set_kind("shared")
+    set_kind(get_config("kind"))
     add_files("src/*.cpp")
+target_end()
 
-target("basic_example")
-    set_kind("binary")
-    add_deps("pigeon_engine")
-    add_files("examples/basic_example.cpp")
+if get_config("examples") == true then
+    target("basic_example")
+        set_kind("binary")
+        add_deps("pigeon_engine")
+        add_files("examples/basic_example.cpp")
+    target_end()
+end
+
+if get_config("test") == true then
+    -- TODO: add test target
+end
