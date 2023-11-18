226463d419fe4bfcb28d0d27713967acd82aee19
Parallel-Paradox <81791934+Parallel-Paradox@users.noreply.github.com>
2023-11-18 20:40:59 +0800

    Execute serial tasks.

--- src/pigeon_framework/task/serial_tasks.cpp
+++ src/pigeon_framework/task/serial_tasks.cpp
@@ -3,6 +3,14 @@
 using namespace pigeon;
 
 Task::Status SerialTasks::Execute() {
-  // TODO: execute tasks serial.
-  return Status::Done;
+  size_t size = tasks_.Size();
+  size_t tail(0);
+  for (size_t i = 0; i < size; ++i) {
+    if (tasks_[i]->Execute() == Status::Keep) {
+      tasks_.Swap(i, tail);
+      ++tail;
+    }
+  }
+  tasks_.Resize(tail);
+  return tail == 0 ? Status::Done : Status::Keep;
 }
